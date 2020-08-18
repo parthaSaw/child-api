e0be2e5ae6790c4226084e538fe93a40668ded9d
Parth <parth@analyticssaves.com>
2020-08-18 09:36:37 +0530

    test

---
+++ auth.js
@@ -0,0 +1,25 @@
+const axios = require('axios')
+
+module.exports=(req,res,next)=>{
+    data = {
+        uname: req.headers.uname,
+        pass: req.headers.pass
+    }
+    
+    if(req.method == "POST"){
+        data.isTransaction = true
+    }
+
+    axios.post('http://localhost:3000/login',data)
+    .then((data)=>{
+        data = data.data
+        if(data.status){
+            next()
+        }else{
+            res.send(data.msg)
+        }
+    },(err)=>{
+        console.log(err)
+    })
+    .catch((err)=>console.log(err))
+}
---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var checkAuth = require('./auth')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/s1', checkAuth,(req, res) => {
+    res.send("DATA FROM S1")
+})
+
+app.post('/s1', checkAuth,(req, res) => {
+    res.send("Transaction on  S1")
+})
+
+console.log("S1 - localhost:3001")
+
+app.listen(3001);
